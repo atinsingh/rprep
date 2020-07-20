9b591eb44eba8b7304b1f7f68530d90149e74df2
Singh, Atin <>
2020-07-20 13:41:55 -0400

    Added Cert Info

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,34 @@
 require('dotenv').config({ path: '.env' });
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger, ValidationPipe, BadRequestException } from '@nestjs/common';
+import { BadRequestException, Logger, ValidationPipe } from '@nestjs/common';
 import { setupSwagger } from './swagger.config';
 import { config } from './app.config';
+import * as fs from 'fs';
+
 const logger: Logger = new Logger('Main');
 const port = process.env.NODE_SERVER_PORT || config.get('server.port');
 
 
 async function bootstrap() {
-  const appOptions = { cors: true };
+
+
+  const certFile = fs.readFileSync('/etc/letsencrypt/live/pragra.io/fullchain.pem');
+  const keyFile = fs.readFileSync('/etc/letsencrypt/live/pragra.io/privkey.pem');
+  const appOptions = {
+    cors: true,
+    httpsOptions: {
+      key: keyFile,
+      cert: certFile,
+    },
+  };
+
   const app = await NestFactory.create(AppModule, appOptions);
 
   app.useGlobalPipes(
     new ValidationPipe({
-      exceptionFactory: (): BadRequestException => new BadRequestException('Validation error')
+      exceptionFactory: (): BadRequestException => new BadRequestException('Validation error'),
     }),
-
   );
 
 
